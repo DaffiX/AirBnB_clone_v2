d5932fc3c68c87f05538f435d1b176241faab747
DaffiX <suleydaffy@gmail.com>
2023-04-10 02:38:25 +0000

    Compress before Deploying

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive from the
-    contents of the web_static folder of the AirBnB Clone repo
-    using the function do_pack
-"""
+#Fabric script that generates a .tgz archive from the
+#contents of the web_static folder of the AirBnB Clone repo
+#using the function do_pack
+
 import os.path
 from datetime import datetime
 from fabric.api import local
